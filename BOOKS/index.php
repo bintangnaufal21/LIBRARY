b31498f57a2fa2cc70c73a22e2181740174f13c5
bintangnaufal21 <105490378+bintangnaufal21@users.noreply.github.com>
2024-01-13 23:34:19 +0700

    t

--- BOOKS/index.php
+++ BOOKS/index.php
@@ -3,26 +3,32 @@
     <table border="2" cellspacing="0" cellpadding="10" align="center" width="800">
         <tr>
             <th>id</th>
-            <th>name</th>
-            <th>category</th>
-            <th>author</th>
-            <th>publisher</th>
-            <th>year</th>
+            <th>Judul Buku</th>
+            <th>NUP</th>
+            <th>Kode Klasifikasi</th>
+            <th>Pengarang</th>
+            <th>Penerbit</th>
+            <th>Tahun Terbit</th>
+            <th>Sumber Buku</th>
+            <th>Jumlah</th>
             <th colspan="2">Action</th>
         </tr>
 
         <?php
         $no=1;
-        $result =$connection -> query("SELECT *FROM books");
+        $result =$connection -> query("SELECT *FROM daftar_buku");
         while( $row = $result->fetch_object() ) {
             ?>
          <tr>
             <td> <?php echo $no?></td>
-            <td> <?php echo $row->name?></td>
-            <td> <?php echo $row->category?></td>
-            <td> <?php echo $row->author?></td>
-            <td> <?php echo $row->publisher?></td>
-            <td> <?php echo $row->year?></td>
+            <td> <?php echo $row->Judul_Buku?></td>
+            <td> <?php echo $row->Nup?></td>
+            <td> <?php echo $row->Kode_Klasifikasi?></td>
+            <td> <?php echo $row->Pengarang?></td>
+            <td> <?php echo $row->Penerbit?></td>
+            <td> <?php echo $row->Tahun_Terbit?></td>
+            <td> <?php echo $row->Sumber_Buku?></td>
+            <td> <?php echo $row->Jumlah?></td>
             <td>
             <a href="index.php?page=books/edit.php&i=<?php echo $row->id?>">edit</a>
             <a href="index.php?page=books/delete.php&i=<?php echo $row->id?>">delete</a>
--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-$connection = mysqli_connect("localhost","root","","library");
+$connection = mysqli_connect("localhost","root","","perpustakaan");
 ?>
